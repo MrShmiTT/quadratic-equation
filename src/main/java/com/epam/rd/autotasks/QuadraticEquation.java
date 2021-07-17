ae3b4ea77a137c5db9026a5813e77ac74e720cf0
Aleksandr Vatutin <shmitt87@mail.ru>
2021-07-17 14:06:47 +0300

    check pls

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -11,8 +11,24 @@ public class QuadraticEquation {
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
 
+        double d = b * b - 4.0 * a * c;
 
+        if (d> 0.0)
+        {
+            double x1 = (-b + Math.sqrt(d)) / (2 * a);
+            double x2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.print(x1 + x2);
+        }
 
+        else if (d == 0.0)
+        {
+            double x1 = -b / (2 * a);
+            System.out.print(x1);
+
+        }
+        else
+        {
+            System.out.println("no roots");
+        }
     }
-
 }
